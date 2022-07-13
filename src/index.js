ff6ec05798368fa5d300327c52991205b52807f6
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2022-07-14 03:20:12 +0600

    Done upto 2:12 or so.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React from "react"
 import ReactDOM from 'react-dom';
 // import ReactDOM from 'react-dom/client';  //React 18 Syntax
 
+// CSS
+import './index.css'
+// VARIABLES
+// these variable will still be available to the entire file so it will render if called e.g {author} or {title}
+const title = "Some Book You Wanna Read"
+const author = "Some Author Who Wrote The Book"
+const image = "http://via.placeholder.com/250x360?text=Visit+WhoIsHostingThis.com+Buyers+Guide"
+
 // stateless functional component
 // always returns JSX
 
@@ -30,9 +38,51 @@ import ReactDOM from 'react-dom';
 // root.render(<App tab="home" />);                                     //React 18 Syntax
 
 function Booklist(){
-  return (
-    <section> This is a booklist </section>
-  )
+   return (
+      <section className="booklist">
+         <Book job="developer"/>
+         <Book title="Some Title" number={22} /> {/* Inside Book component, 1st prop is rendered as string. 2nd prop is rendered as a number. */}
+         <Book />
+      </section>
+   )
+}
+
+const Book = (props) => {
+   console.log(props) // first way to console log
+   return (
+      <article className="book">
+         This is a booklist
+         <br></br>
+         <Image />
+         <Title />
+         <Author />
+         {console.log(props)}  {/* second way to console log */}
+      </article>
+   )
+};
+
+const Image = () => {
+   return (
+      <img src={image} alt="" />
+   )
+}
+
+const Title = () => {
+   return (
+      <h1> {title} </h1>
+   )
+}
+
+// the style uses double curly braces for a reason. The first one is to denote that it contains JS inside it. The 2nd is an ojbect we are passing to
+// the tag as JS properties. Also notice the lack of double quotes after the = sign. Its the norm for dealing with pure JS.
+const Author = () => {
+   return (
+      <div>
+         <h6 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem' }}> {author.toUpperCase()} </h6>
+         {/* let x = 12 (Throws error since it doesn't return anything) */}
+         <p>{6 + 6}</p>
+      </div>
+   )
 }
 
 ReactDOM.render(<Booklist />, document.getElementById('root'))
