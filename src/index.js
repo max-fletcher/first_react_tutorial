ad95375beba9099f7d0b56cd732cfae7b97061c4
Mahin Chowdhury <amar.naam.machine@gmail.com>
2022-07-13 19:52:31 +0600

    Progressed to about 1:40.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom/client';  //React 18 Syntax
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// stateless functional component
+// always returns JSX
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// JSX rules
+// return single element(just like vue parent div)
+// div/section/article or Fragment
+// use camelCase for html attributes (e.g use onClick="" instead of onclick="")
+// className instead of class (e.g className="mx-3 my-2 d-flex", instead of class="mx-3 d-flex")
+// close every element (e.g you can't do <img src="" alt="">, you have to use <img src="" alt="" /> to the close tag properly. Another example is <input type="button" value="" /> and not <input type="button" value="">).
+// formatting (e.g use opening and closing brackets when returning JSX e.g return ( <div> <h4>This is my first component</h4> </div> ) )
+
+// function Greeting(){
+//   return (
+//     <div>
+//       <Person />
+//       <Message />
+//     </div>
+//   )
+// }
+
+// const Person = () => <h1>My name is John Doe</h1>
+// const Message = () => <h4>This is my first component</h4>
+
+// ReactDOM.render(<Greeting />, document.getElementById('root'))
+// const root = ReactDOM.createRoot(document.getElementById('app'));    //React 18 Syntax
+// root.render(<App tab="home" />);                                     //React 18 Syntax
+
+function Booklist(){
+  return (
+    <section> This is a booklist </section>
+  )
+}
+
+ReactDOM.render(<Booklist />, document.getElementById('root'))
